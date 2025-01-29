089e5c10f0a0f81bbc66c510bda2348336e8dbce
Vaibhav Nayan <vaibhavron10@gmail.com>
2025-01-29 16:39:23 +0530

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,31 @@
+import Image from 'next/image';
+import Sidebar from '../../components/Sidebar'
+import MobileNav from '@/components/MobileNav';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName: 'Vaibhav', lastName: 'Nayan'};
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+
+        <div className='flex size-full flex-col'>
+          <div className='root-layout'>
+            <Image
+              src='/icons/logo.svg'
+              width={30}
+              height={30}
+              alt='menu icon'
+            />
+            <div>
+              <MobileNav user={loggedIn}/>
+            </div>
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,56 @@
+'use client'
+import Image from 'next/image'
+import React from 'react'
+import Link from 'next/link'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
+
+const Sidebar = ({user}: SiderbarProps) => {
+    const pathname = usePathname();
+  return (
+    <section className='sidebar'>
+        <nav className='flex flex-col gap-4'>
+            <Link href="/" className="flex mb-12 cursor-pointer gap-2 items-center">
+                <Image 
+                    src="/icons/logo.svg"
+                    width={34}
+                    height={34}
+                    alt='Heritage logo'
+                    className='size=[24px] max-xl:size-14'
+                />
+                <h1 className='sidebar-logo'>Heritage</h1>
+            </Link>
+
+            {sidebarLinks.map((item)=>{
+                const isActive = pathname === item.route || pathname.startsWith(`${item.route}`)
+                return (
+                    <Link href={item.route} key={item.label} className={cn('sidebar-link', {
+                        'bg-bankGradient' : isActive
+                    })}>
+                        <div className='relative size-6'>
+                            <Image
+                                src={item.imgURL}
+                                alt={item.label}
+                                fill
+                                className={cn({
+                                    'brightness-[3] invert-0': isActive
+                                })}
+                            />
+                        </div>
+                        <p className={cn('sidebar-label',{
+                            '!text-white' : isActive}
+                        )}>
+                            {item.label}
+                        </p>
+                    </Link>
+                )
+            })}
+            USER
+        </nav>
+        FOOTER
+    </section>
+  )
+}
+
+export default Sidebar
